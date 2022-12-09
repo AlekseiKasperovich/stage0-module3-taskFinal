356d6d98a6573c32ad90eee45614b55edfa4ad14
AlekseiKasperovich <AleksKasperovich@yandex.by>
2022-12-09 13:22:19 +0300

    commit

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -1,6 +1,14 @@
 package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
+
     public void calculateSum(int number) {
+        int res = 0;
+        while (number != 0) {
+            int temp = number % 10;
+            number = number / 10;
+            res += temp;
+        }
+        System.out.println(res);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -1,6 +1,14 @@
 package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
+
     public void revert(int number) {
+        String res = "";
+        while (number != 0) {
+            int temp = number % 10;
+            number = number / 10;
+            res += temp;
+        }
+        System.out.println(res);
     }
 }
